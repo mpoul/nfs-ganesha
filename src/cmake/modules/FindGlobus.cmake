a423adc4e68b5e27af70e95f1386113602dd1753
mpoul <poul@mail.muni.cz>
2013-12-08 20:39:51 +0100

    Add cmake module placeholder to search for globus library

---
+++ src/cmake/modules/FindGlobus.cmake
@@ -0,0 +1,5 @@
+# - Find Globus library
+# Find the native Globus headers and libraries.
+#  GLOBUS_INCLUDE_DIRS      - where to find globus, etc.
+#  GLOBUS_LIBRARIES         - List of libraries when using globus.
+#  GLOBUS_FOUND             - True if globus found.
